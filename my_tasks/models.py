70cda20061f0787b27fe4087587308ef175ea7f5
garymast <>
2023-12-16 08:34:32 +0000

    Add additional model fields

--- my_tasks/models.py
+++ my_tasks/models.py
@@ -12,9 +12,23 @@ class Post(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
     )
-
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     due_date = models.DateTimeField()
     done = models.BooleanField(default=False)
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+# The on_delete argument emulates the behaviour of an SQL database when
+# an author is deleted. As we have set this to models.CASCADE: if a user
+# is deleted, then any posts they have authored will also be deleted.
+# If, instead, you wanted to delete each post manually if their author
+# was deleted, then you could set this to RESTRICT. It is important
+# that on_delete is used as it will ensure database integrity.
+# ***Maybe change this to RESTRICT
+
+# The related_name parameter is optional but is useful as it gives
+# a meaningful name to the relation from the User model to the Post.
+# Without it, Django would autogenerate the name of author_set for a
+# query of a user's posts, which is not as semantic as blog_posts.
