a75b113646feea27816d405f0eb74652ab08a624
garymast <>
2023-12-16 06:08:40 +0000

    Add hidden secret keys

--- flamenco/settings.py
+++ flamenco/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yc6@_ls)(ve8!k8d-p$*kdmetz1jtgf-89vo288n51^ir!@m31'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
