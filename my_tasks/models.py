93b7e29109c135effaf7d9df851f48aab7a06c61
garymast <>
2023-12-16 22:16:54 +0000

    Add registration functionality

--- my_tasks/models.py
+++ my_tasks/models.py
@@ -9,9 +9,9 @@ PRIO = ((0, "Low"), (1, "Medium"), (2, "High"))
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
+    # slug = models.SlugField(max_length=200, null=True, blank=True)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="blog_posts"
+        User, on_delete=models.CASCADE, null=True, blank=True
     )
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
--- my_tasks/views.py
+++ my_tasks/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
-from django.contrib.auth.views import LoginView
 
+from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
 from .models import Post
 
 # from django.http import HttpResponse
@@ -22,9 +25,32 @@ class CustomLoginView(LoginView):
         return reverse_lazy('tasks')
 
 
+class RegisterPage(FormView):
+    template_name = 'my_tasks/signup.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        author = form.save()
+        if author is not None:
+            login(self.request, author)
+        return super(RegisterPage, self).form_valid(form)
+
+
 class TaskList(LoginRequiredMixin, ListView):
     model = Post
     context_object_name = 'tasks'
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['tasks'] = context['tasks'].filter(author=self.request.user)
+    #     context['count'] = context['tasks'].filter(done=False).count()
+
+    #     return context
+
+
+# Look at creating user groups in the future
 
 
 class TaskDetail(LoginRequiredMixin, DetailView):
@@ -35,15 +61,19 @@ class TaskDetail(LoginRequiredMixin, DetailView):
 
 class TaskCreate(LoginRequiredMixin, CreateView):
     model = Post
-    fields = '__all__'
+    fields = ['title', 'content', 'done', 'priority', 'due_date']
     # Also look at adding form class as per CodeInstitute Hello Django
     success_url = reverse_lazy('tasks')
     # 2013-06-26 00:14:26.260524 Example Date Time String
+    
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TaskCreate, self).form_valid(form)
 
 
 class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Post
-    fields = '__all__'
+    fields = ['title', 'content', 'done', 'priority', 'due_date']
     success_url = reverse_lazy('tasks')
 
 
